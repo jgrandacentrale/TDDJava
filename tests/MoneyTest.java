bd39609363b2206f84eeae88d3defc5126eba48e
jgrandacentrale <jeremy.grand@centrale-marseille.fr>
2015-01-18 16:29:58 +0100

    True commit Chapter 11 : shaving duplications

--- tests/MoneyTest.java
+++ tests/MoneyTest.java
@@ -21,13 +21,7 @@ public class MoneyTest {
         assertFalse( Money.dollar(5).equals(Money.dollar(6)));
         assertTrue(  Money.franc(5).equals(Money.franc(5)));
     }
-
-    @Test
-    public void testFrancMultiplication(){
-        Money five = Money.franc(5);
-        assertEquals(  Money.franc(10),five.times(2));
-        assertEquals(  Money.franc(15),five.times(3));
-    }
+    
 
     @Test
     public void testCurrency(){
