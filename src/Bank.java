6ef7cc2c1590ec9064042880141918ac82b0cb07
jgrandacentrale <jeremy.grand@centrale-marseille.fr>
2015-01-18 17:29:46 +0100

    Chapter 14 : Growing a Pair

--- src/Bank.java
+++ src/Bank.java
@@ -1,10 +1,26 @@
+import java.util.Hashtable;
+
 /**
  * Created by Skyro on 18/01/2015.
  */
 public class Bank {
 
-    Money reduce(Expression source, String to){
-        
-        return source.reduce(to);
+    private Hashtable rates = new Hashtable();
+
+    int rate(String from, String to){
+        if(from.equals(to)) return 1;
+        Integer rate = (Integer) rates.get(new Pair(from,to));
+        return rate.intValue();
     }
+
+    Money reduce(Expression source, String to){
+
+        return source.reduce(this,to);
+    }
+
+    public void addRate(String from, String to, int rate) {
+        rates.put(new Pair(from,to),new Integer(rate));
+    }
+
+
 }
